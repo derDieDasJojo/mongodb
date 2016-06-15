b247e79952bf6a968e55919da5aac3130eb764b2
Your Name <you@example.com>
2016-06-15 14:48:26 +0000

    work in progress

--- mongod.sh
+++ mongod.sh
@@ -14,8 +14,8 @@ DBAUTH=""
 BACKGROUND="--fork --logpath $MONGO_LOG" 
 
 # start mongod
-$MONGOD $BACKGROUND --replSet $MONGO_RS --smallfiles 				#without authentication config
-#$MONGOD $BACKGROUND --replSet $MONGO_RS --smallfiles --config /etc/mongod.conf #with authentication config
+#$MONGOD $BACKGROUND --replSet $MONGO_RS --smallfiles 				#without authentication config
+$MONGOD $BACKGROUND --replSet $MONGO_RS --smallfiles --config /etc/mongod.conf #with authentication config
  
 # not used function
 checkSlaveStatus(){
@@ -49,57 +49,53 @@ echo "hostname: ${HOSTNAME}"
 echo "mongo_master: ${MONGO_MASTER}" 
 if [[ "$HOSTNAME" == "$MONGO_MASTER" ]]
 then
-        # initiate cluster
-	echo "I am The master. I initiate the cluster"
-	$MONGO --eval "rs.initiate()"
-	$MONGO  --eval "rs.status()"
+  # initiate cluster
+  echo "I am The master. I initiate the cluster"
+  $MONGO --eval "rs.initiate()"
+  $MONGO  --eval "rs.status()"
         
   echo "Set up authentication and restart mongod"
   sleep 3 #for the replicaset to have a short time to initialize
   $MONGO admin --eval "clusteradminpassword=\"${CLUSTER_ADMIN_PASS}\"" app/createClusterAdmin.js
-  $MONGO $DBNAME --eval "adminuser=\"${DB_ADMIN_USER}\", adminpassword=\"${DB_ADMIN_PASS}\"" app/createAdmin.js
+  $MONGO admin -u "clusterAdmin" -p "${CLUSTER_ADMIN_PASS}" --eval "adminuser=\"${DB_ADMIN_USER}\", adminpassword=\"${DB_ADMIN_PASS}\", database=\"${DBNAME} \"  " app/createAdmin.js
   #$MONGO -u "clusterAdmin" -p "${CLUSTER_ADMIN_PASS}" $DBNAME --eval "adminuser=\"${DB_ADMIN_USER}\", adminpassword=\"${DB_ADMIN_PASS}\"" app/createAdmin.js #if authentication is needed
-  $MONGOD $BACKGROUND --replSet $MONGO_RS --smallfiles --config /etc/mongod.conf
-        
+  #$MONGOD $BACKGROUND --replSet $MONGO_RS --smallfiles --config /etc/mongod.conf      
   echo "I introduce myself to the config-server"
-	# wait for mongo master
-	waitFor $MONGO_ROUTER 27017
-	#$MONGO $MONGO_ROUTER:27017 --eval "sh.addShard(\"rs1/$HOSTNAME:27017\")"
-	$MONGO -u clusterAdmin -p ${CLUSTER_ADMIN_PASS} --authenticationDatabase admin $MONGO_ROUTER:27017 --eval "sh.addShard(\"rs1/$HOSTNAME:27017\")"
-	if [ $? == 0 ]; then
+  # wait for mongo master
+  waitFor $MONGO_ROUTER 27017
+  #$MONGO $MONGO_ROUTER:27017 --eval "sh.addShard(\"rs1/$HOSTNAME:27017\")"
+  $MONGO -u clusterAdmin -p ${CLUSTER_ADMIN_PASS} --authenticationDatabase admin $MONGO_ROUTER:27017 --eval "sh.addShard(\"rs1/$HOSTNAME:27017\")"
+  if [ $? == 0 ]; then
     echo success
   else
-	  echo "failed to addShard to router. Exiting"
-		exit -1
-	fi
+    echo "failed to addShard to router. Exiting"
+    exit -1
+  fi
 else
-	# add mongod to cluster
-	echo "I am a Slave ${HOSTNAME}. I join the cluster of $MONGO_MASTER"
-	#wait 5 secs to the master has time
-	sleep 5
-	# check again if the assumed master is really a master
-        #echo $MONGO $DBAUTH ${MONGO_MASTER}/admin -- eval "printjson(db.isMaster())"   
-	IS_MASTER=`mongo $DBAUTH ${MONGO_MASTER}/admin --eval "printjson(db.isMaster())" | grep 'ismaster'`
-	if echo $IS_MASTER | grep "true"; then
-		# if it is a master
-		# check if mongod is already member of this cluster
-		IS_MEMBER=`mongo $DBAUTH ${MONGO_MASTER}/admin --eval "printjson(rs.conf())" | grep "$HOSTNAME"`
-		if echo $IS_MEMBER | grep "$HOSTNAME"; then
-			echo "I am already member of the cluster. So I will do nothing."
-		else
-			echo "I am not jet member of the cluster. So i will join"
-			echo $MONGO $DBAUTH ${MONGO_MASTER}/admin --eval "rs.add(\"${HOSTNAME}:27017\")"	
-			$MONGO $DBAUTH ${MONGO_MASTER}/admin --eval "rs.add(\"${HOSTNAME}:27017\")"
-			$MONGO $DBAUTH ${MONGO_MASTER}/admin --eval "rs.status()"
-		fi
-	else	
-		# if its not a master, something is wrong
-		echo "The assumed Master($MONGO_MASTER) is not a master."
-		echo "I refuse to enslave myself, if its not a master. I kill myself"
-		exit 1 #exit with error
-	fi
-	
+  # add mongod to cluster
+  echo "I am a Slave ${HOSTNAME}. I join the cluster of $MONGO_MASTER"
+  #wait 5 secs to the master has time
+  sleep 5
+  # check again if the assumed master is really a master
+  #echo $MONGO $DBAUTH ${MONGO_MASTER}/admin -- eval "printjson(db.isMaster())"   
+  IS_MASTER=`mongo $DBAUTH ${MONGO_MASTER}/admin --eval "printjson(db.isMaster())" | grep 'ismaster'`
+  if echo $IS_MASTER | grep "true"; then
+    # if it is a master
+    # check if mongod is already member of this cluster
+    IS_MEMBER=`mongo $DBAUTH ${MONGO_MASTER}/admin --eval "printjson(rs.conf())" | grep "$HOSTNAME"`
+    if echo $IS_MEMBER | grep "$HOSTNAME"; then
+      echo "I am already member of the cluster. So I will do nothing."
+    else
+      echo "I am not jet member of the cluster. So i will join"
+      echo $MONGO $DBAUTH ${MONGO_MASTER}/admin --eval "rs.add(\"${HOSTNAME}:27017\")"	
+      $MONGO $DBAUTH ${MONGO_MASTER}/admin --eval "rs.add(\"${HOSTNAME}:27017\")"
+      $MONGO $DBAUTH ${MONGO_MASTER}/admin --eval "rs.status()"
+    fi
+  else	
+    # if its not a master, something is wrong
+    echo "The assumed Master($MONGO_MASTER) is not a master."
+    echo "I refuse to enslave myself, if its not a master. I kill myself"
+    exit 1 #exit with error
+  fi
 fi
-#tailf /dev/null
-echo "start mongo in foreground"
-$MONGOD  --replSet $MONGO_RS --smallfiles --config /etc/mongod.conf #with authentication config
+tailf $MONGO_LOG
